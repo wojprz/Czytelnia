0f26b4f3ce35d0429e8d5badc5069ff90eeb33bc
Wojciech Przybyła <wojprz@st.amu.edu.pl>
2022-08-08 00:57:36 +0200

    connection string

---
+++ Czytelnia/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Czytelnia.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Czytelnia.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :  base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Czytelnia/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Czytelnia.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; } = 0;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
